71e720e0f48b97ffd62f70a45511bc94030ba736
Harlequine <cs.joshuakylepocampo@gmail.com>
2024-01-05 16:15:33 +0800

    Initial Commit

---
+++ src/main/java/com/magpie/magpiecodingchallenge/MagpieCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.magpie.magpiecodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagpieCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagpieCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magpie/magpiecodingchallenge/MagpieCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magpie.magpiecodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagpieCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
